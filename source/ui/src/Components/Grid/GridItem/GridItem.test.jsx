2a5b7572cd107647780ddee78259e7581ea40e70
Corey Rodems <corey@rodems.dev>
2020-11-22 16:48:18 -0500

    workin on tests

--- source/ui/src/Components/Grid/GridItem/GridItem.test.jsx
+++ source/ui/src/Components/Grid/GridItem/GridItem.test.jsx
@@ -34,3 +34,38 @@ describe('Grid Item Tests', () => {
     });
   });
 });
+
+describe('Grid Item Tests', () => {
+  afterEach(cleanup);
+
+  describe.each`
+    breakpoint
+    ${'smPhone'}
+    ${'phone'}
+    ${'tablet'}
+    ${'tabletHz'}
+    ${'smDesktop'}
+    ${'desktop'}
+    ${'lgDesktop'}
+  `('GridItem: $breakpoint breakpoint', ({ breakpoint }) => {
+    test.each`
+      prop          | val
+      ${'colStart'} | ${1}
+      ${'colSpans'} | ${1}
+      ${'rowStart'} | ${1}
+      ${'rowSpan'}  | ${1}
+    `('$prop: $val', ({ prop, val }) => {
+      const props = { [breakpoint]: { [prop]: val } };
+      const innerText = `griditem ${breakpoint}`;
+      render(
+        <Grid>
+          <GridItem {...props} subGrid>
+            {innerText}
+          </GridItem>
+        </Grid>,
+      );
+      const gridItem = screen.getByText(innerText);
+      expect(gridItem).toBeInTheDocument();
+    });
+  });
+});
